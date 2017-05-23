e408d2a6218a505e315845a4a137374cb1d62df4
juan <feanorjm@gmail.com>
2017-05-22 21:56:18 -0400

    creación app maintenance

---
+++ maintenance/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from maintenance.models import UsuarioComp,Compania,Clasificacion_maquina,Maquina,Detalle_Maquina
+
+class UsuarioCompAdmin(admin.ModelAdmin):
+    list_display = ('user','tipo')
+    list_filter = ['user','tipo']
+
+class CompaniaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','numero')
+    list_filter = ['nombre','numero']
+
+class ClasificacionAdmin(admin.ModelAdmin):
+    list_display = ('nombre','descripcion')
+    list_filter = ['nombre','descripcion']
+
+class MaquinaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','clasificacion','compania')
+    list_filter = ['nombre','clasificacion','compania']
+
+admin.site.register(UsuarioComp,UsuarioCompAdmin)
+admin.site.register(Compania,CompaniaAdmin)
+admin.site.register(Clasificacion_maquina,ClasificacionAdmin)
+admin.site.register(Maquina,MaquinaAdmin)
+
+'''admin.site.register(TblComuna,ComunaAdmin)
+admin.site.register(TblProvincia,ProvinciaAdmin)
+admin.site.register(TblRegion,RegionAdmin)
+'''
---
+++ maintenance/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from smart_selects.db_fields import ChainedForeignKey
+from django.contrib.auth.models import User
+
+TIPO_USER_CHOICES = (
+    ('0', 'Teniente de máquina'),
+    ('1', 'OBAC'),
+    ('2', 'Inspector general de máquina'),
+    ('3', 'Comandante')
+)
+
+class UsuarioComp(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=1, choices=TIPO_USER_CHOICES)
+
+    def __str__(self):
+        return self.tipo
+
+class Compania(models.Model):
+    numero = models.IntegerField()
+    nombre = models.CharField(max_length=45)
+    direccion = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nombre
+
+class Clasificacion_maquina(models.Model):
+    nombre = models.CharField(max_length=45)
+    descripcion = models.CharField(max_length=100,null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Maquina(models.Model):
+    nombre = models.CharField(max_length=45)
+    clasificacion = models.ForeignKey(Clasificacion_maquina)
+    compania = models.ForeignKey(Compania)
+    numero_motor = models.CharField(max_length=45)
+    numero_chasis = models.CharField(max_length=45)
+    bin = models.CharField(max_length=45)
+    patente = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
+
+class Detalle_Maquina(models.Model):
+    maquina = models.ForeignKey(Maquina)
+    venc_patente = models.DateField()       #vencimiento patente
+    costo_patente = models.IntegerField()   #costo patente
+    soap_costo = models.IntegerField()      #seguro obligatorio
+    venc_rev_tec = models.DateField()       #vencimiento revicion tecnica
+    costo_rev_tec = models.IntegerField()   #costo revision tecnica
+    costo_seg_auto = models.IntegerField()  #costo seguro automotriz
+
